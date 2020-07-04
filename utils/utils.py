e7f1d432dd4e8e30d4ad585a0c01204b37ce4053
islootski <ilia@vastdata.com>
2020-07-04 13:35:03 +0300

    add log_dir

--- utils/utils.py
+++ utils/utils.py
@@ -1,12 +1,14 @@
 
 import logging
+import os
+
+LOG_DIR = "log_dir"
 
 
 def init_log():
     from datetime import datetime
     date_time = datetime.now().strftime("%d_%m_%H%M")
-    LOG_FILENAME = f'{date_time}_.log'
-    print(LOG_FILENAME)
-    logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s',
+    file_name = os.path.join(os.path.realpath('.').split("tests")[0], LOG_DIR, f'{date_time}_.log')
+    logging.basicConfig(filename=file_name, level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s',
                         datefmt='%d/%m/%YT%H:%M:%S')
     return logging
